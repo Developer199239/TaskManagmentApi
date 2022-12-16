4f05e7d47df56effc52a9eaf1ba3febb2a3dbc75
Murtuza Rahman <murtuza.rahman83@gmail.com>
2022-12-16 14:33:20 +0600

    first commit

---
+++ src/tasks/pipes/task-status-validation.pipe.ts
@@ -0,0 +1,23 @@
+import { ArgumentMetadata, BadRequestException, PipeTransform } from "@nestjs/common";
+import { TaskStatus } from "../task.model";
+
+export class TaskStatusValidationPipe implements PipeTransform {
+    readonly allowedStauses = [
+        TaskStatus.OPEN,
+        TaskStatus.IN_PROGRESS,
+        TaskStatus.DONE
+    ];
+    transform(value: any) {
+        value = value.toUpperCase();
+        if(!this.isStatusValid(value)) {
+            throw new BadRequestException(`"${value}" is an invalid staus`);
+        }
+        return value;
+    }
+
+    private isStatusValid(status: any){
+        const idx = this.allowedStauses.indexOf(status);
+        return idx !== -1;
+    }
+
+}
